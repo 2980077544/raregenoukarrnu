edb3b980e4fd9b1495bdead5f37fe05535b958a2
yuanwow <2980077544@qq.com>
2023-07-01 12:22:53 +0800

    修复2

--- redirect.js
+++ redirect.js
@@ -14,37 +14,38 @@ var $_GET = (function () {
   }
 })();
 function checkWebSiteOnline(config) {
-  function getURL(url) {
-    function getXmlHttpRequest() {
-      if (window.XMLHttpRequest) {
-        return new XMLHttpRequest();
-      } else if (window.ActiveXObject) {
-        return new ActiveXObject("Microsoft.XMLHTTP");
-      }
-    }
-    var xmlhttp = getXmlHttpRequest();
-    xmlhttp.open("GET", url, false); //第三个参数表示是否异步
-    xmlhttp.send();
-    if (xmlhttp.readyState == 4) {
-      if (xmlhttp.status == 200) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-  getURL(config.url)&&config.success()||config.ertor();
+  var img = new Image();
+  img.onload = function () {
+    if (typeof config.success == "function") config.success(config.url);
+  };
+  img.onerror = function () {
+    if (typeof config.error == "function") config.error(config.url);
+  };
+  img.src = config.url + (config.isImage ? "" : "/favicon.ico");
 }
 $_GET["keep"] != "1" &&
   checkWebSiteOnline({
-    url: "https://blog.genouka.rr.nu",
+    url: "https://blog.genouka.rr.nu/_next/image?url=%2Fassets%2Fsocial%2Fbilibili.svg&w=1080&q=75",
+    isImage: true,
     success: function (url) {
-      window.confirm("Rare系列的新网站可以访问，要跳转吗？") &&
+      //window.confirm("Rare系列的新网站可以访问，要跳转吗？") &&
+      //window.location.replace("https://blog.genouka.rr.nu/Rare-xi-lie-ye-mian");
+      mdui.confirm("点击确定前往新网站","新网站已开通！", function () {
         window.location.replace(
           "https://blog.genouka.rr.nu/Rare-xi-lie-ye-mian"
         );
+      });
     },
     error: function (url) {
-      alert(url + "无法访问");
+      //alert(url + "无法访问");
+      mdui.snackbar({
+        message: "⚠️新网站无法访问",
+        buttonText: "仍然访问",
+        onButtonClick: function () {
+          window.location.replace(
+            "https://blog.genouka.rr.nu/Rare-xi-lie-ye-mian"
+          );
+        },
+      });
     },
   });
